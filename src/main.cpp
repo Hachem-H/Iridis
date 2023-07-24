66159200f1f13dd25474ade014e778851631dfe7
Hachem-H <hh.naram@gmail.com>
2023-07-25 01:39:23 +0300

    Start work on the parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,28 @@
+#include <optional>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::optional<std::string> ReadFile(const char* path)
+{
+    std::ifstream file(path);
+
+    if (!file)
+        return std::nullopt;
+
+    file.seekg(0, std::ios::end);
+    const std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::string content;
+    content.reserve(size);
+    content.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+
+    return content;
+}
+
 int main()
 {
-    
+    if (auto file = ReadFile("examples/Basic Example.iridis"))
+        std::cout << *file << '\n';
 }
